8e113ec167dda4d028ebce0c3a245d8b8ec9a68f
odom <odom.john@hotmail.com>
2012-12-27 22:55:30 +0700

    testing module and change database structure

--- sql/DB_room_management.sql
+++ sql/DB_room_management.sql
@@ -35,25 +35,21 @@ description varchar(255)
 create table resources
 (
 id int(8),
-idCategory int(8)Not Null,
-idQuality int(8)Not Null,
+idCategory int(8)Not Null REFERENCES categories(id) ,
+idQuality int(8)Not Null REFERENCES qualities(id),
 numbers int(8),
-description varchar(255),
-FOREIGN KEY (idCategory) REFERENCES categories(id)ON UPDATE CASCADE ON DELETE CASCADE,
-FOREIGN KEY (idQuality) REFERENCES qualities(id)ON UPDATE CASCADE ON DELETE CASCADE
+description varchar(255)
 );
 
 
 create table `book-resource`
 (
-idUser int(8),
-idResource int(8),
+idUser int(8)REFERENCES users(id),
+idResource int(8)REFERENCES resources(id),
 startTime int(8),
 endTime int(8),
 report varchar(255),
-PRIMARY KEY (idUser,idResource),
-FOREIGN KEY (idUser) REFERENCES users(id),
-FOREIGN KEY (idResource) REFERENCES resources(id)
+PRIMARY KEY (idUser,idResource)
 );
 
 create table buildings 
@@ -66,27 +62,19 @@ description varchar(255)
 create table rooms
 (
 id int(8),
-idBuilding int(8),
-idQuality int(2),
+idBuilding int(8)REFERENCES buildings(id),
+idQuality int(2)REFERENCES qualities(id),
 `number` int(8),
 description varchar(255),
-PRIMARY KEY (id),
-FOREIGN KEY (idBuilding) REFERENCES buildings(id),
-FOREIGN KEY (idQuality) REFERENCES qualities(id)
+PRIMARY KEY (id)
 );
 
-create table book-room
+create table `book-room`
 (
-idUser int(8),
-idRoom int(8),
+idUser int(8)REFERENCES users(id),
+idRoom int(8)REFERENCES rooms(id),
 startTime int(8),
 endTime int(8),
 report varchar(255),
-PRIMARY KEY (idUser,idRoom),
-FOREIGN KEY (idUser) REFERENCES users(id),
-FOREIGN KEY (idRoom) REFERENCES rooms(id)
-)
-
-
-
-
+PRIMARY KEY (idUser,idRoom)
+);
