224b7b6c86be2ad923d6b3fa69e530f69eabc88d
SESOM <sesomitc41@gmail.com>
2013-01-07 13:31:14 +0700

    edit file user

--- booking-system/module/Booking/config/module.config.php
+++ booking-system/module/Booking/config/module.config.php
@@ -8,140 +8,138 @@
  * @license   http://framework.zend.com/license/new-bsd New BSD License
  */
 return array(
-	'router' => array(
-		'routes' => array(
-			// Home Register //
-			'home' => array(
-				'type' => 'Zend\Mvc\Router\Http\Literal',
-				'options' => array(
-					'route' => '/',
-					'defaults' => array(
-						'controller' => 'index',
-						'action' => 'index',
-					),
-				),
-				'may_terminate' => true,
-				'child_routes' => array(
-					'user-list' => array(
-						'type' => 'Zend\Mvc\Router\Http\Literal',
-						'options' => array(
-							'route' => 'users',
-							'defaults' => array(
-								'controller' => 'index',
-								'action' => 'showUsers',
-							),
-						),
-					),
-					'rooms-list' => array(
-						'type' => 'Zend\Mvc\Router\Http\Literal',
-						'options' => array(
-							'route' => 'view',
-							'defaults' => array(
-								'controller' => 'index',
-								'action' => 'showRooms',
-							),
-						),
-					),
-				),
-			),
-			// Booking Action //
-			'booking' => array(
-				'type' => 'Zend\Mvc\Router\Http\Literal',
-				'options' => array(
-					'route' => '/booking',
-					'defaults' => array(
-						'controller' => 'Booking\Controller\booking',
-						'action' => 'index',
-					),
-				),
-				'may_terminate' => true,
-				'child_routes' => array(
-					'add_room' => array(
-						'type' => 'Zend\Mvc\Router\Http\Literal',
-						'options' => array(
-							'route' => '/add_room',
-							'defaults' => array(
-								'controller' => 'Booking\Controller\booking',
-								'action' => 'addRoom',
-							),
-						),
-					),
-					'add_user' => array(
-						'type' => 'Zend\Mvc\Router\Http\Literal',
-						'options' => array(
-							'route' => '/add_user',
-							'defaults' => array(
-								'controller' => 'Booking\Controller\booking',
-								'action' => 'addUser',
-							),
-						),
-					),
-				),
-			),
-			
-			// User Management //
-			'users-list' => array(
-				'type' => 'Literal',
-				'options' => array(
-					'route' => '/user',
-					'defaults' => array(
-						'controller' => 'index',
-						'action' => 'showUsers',
-					),
-				),
-				'may_terminate' => true,
-				'child_routes' => array(
-					'default' => array(
-						'type' => 'Segment',
-						'options' => array(
-							'route' => '/[:controller[/:action]]',
-							'constraints' => array(
-								'controller' => '[a-zA-Z][a-zA-Z0-9_-]*',
-								'action' => '[a-zA-Z][a-zA-Z0-9_-]*',
-							),
-							'defaults' => array(
-							),
-						),
-					),
-				),
-			),
-		),
-	),
-	'controllers' => array(
-		'invokables' => array(
-			'index' => 'Booking\Controller\IndexController',
-			'Booking\Controller\Booking' => 'Booking\Controller\BookingController'
-		),
-	),
-	'service_manager' => array(
-		'factories' => array(
-			'translator' => 'Zend\I18n\Translator\TranslatorServiceFactory',
-		),
-	),
+    'router' => array(
+        'routes' => array(
+            // Home Register //
+            'home' => array(
+                'type' => 'Zend\Mvc\Router\Http\Literal',
+                'options' => array(
+                    'route' => '/',
+                    'defaults' => array(
+                        'controller' => 'index',
+                        'action' => 'index',
+                    ),
+                ),
+                'may_terminate' => true,
+                'child_routes' => array(
+                    'user-list' => array(
+                        'type' => 'Zend\Mvc\Router\Http\Literal',
+                        'options' => array(
+                            'route' => 'users',
+                            'defaults' => array(
+                                'controller' => 'index',
+                                'action' => 'showUsers',
+                            ),
+                        ),
+                    ),
+                    'rooms-list' => array(
+                        'type' => 'Zend\Mvc\Router\Http\Literal',
+                        'options' => array(
+                            'route' => 'view',
+                            'defaults' => array(
+                                'controller' => 'index',
+                                'action' => 'showRooms',
+                            ),
+                        ),
+                    ),
+                ),
+            ),
+            // Booking Action //
+            'booking' => array(
+                'type' => 'Zend\Mvc\Router\Http\Literal',
+                'options' => array(
+                    'route' => '/booking',
+                    'defaults' => array(
+                        'controller' => 'booking',
+                        'action' => 'index',
+                    ),
+                ),
+                'may_terminate' => true,
+                'child_routes' => array(
+                    'add_room' => array(
+                        'type' => 'Zend\Mvc\Router\Http\Literal',
+                        'options' => array(
+                            'route' => '/add_room',
+                            'defaults' => array(
+                                'controller' => 'booking',
+                                'action' => 'addRoom',
+                            ),
+                        ),
+                    ),
+                    'add-user' => array(
+                        'type' => 'Zend\Mvc\Router\Http\Literal',
+                        'options' => array(
+                            'route' => '/add-user',
+                            'defaults' => array(
+                                'controller' => 'booking',
+                                'action' => 'addUser',
+                            ),
+                        ),
+                    ),
+                ),
+            ),
+            // User Management //
+            'users-list' => array(
+                'type' => 'Literal',
+                'options' => array(
+                    'route' => '/user',
+                    'defaults' => array(
+                        'controller' => 'index',
+                        'action' => 'showUsers',
+                    ),
+                ),
+                'may_terminate' => true,
+                'child_routes' => array(
+                    'default' => array(
+                        'type' => 'Segment',
+                        'options' => array(
+                            'route' => '/[:controller[/:action]]',
+                            'constraints' => array(
+                                'controller' => '[a-zA-Z][a-zA-Z0-9_-]*',
+                                'action' => '[a-zA-Z][a-zA-Z0-9_-]*',
+                            ),
+                            'defaults' => array(
+                            ),
+                        ),
+                    ),
+                ),
+            ),
+        ),
+    ),
+    'controllers' => array(
+        'invokables' => array(
+            'index' => 'Booking\Controller\IndexController',
+            'booking' => 'Booking\Controller\BookingController'
+        ),
+    ),
+    'service_manager' => array(
+        'factories' => array(
+            'translator' => 'Zend\I18n\Translator\TranslatorServiceFactory',
+        ),
+    ),
     'translator' => array(
         'locale' => 'en_US',
         'translation_file_patterns' => array(
             array(
-                'type'     => 'gettext',
+                'type' => 'gettext',
                 'base_dir' => __DIR__ . '/../language',
-                'pattern'  => '%s.mo',
+                'pattern' => '%s.mo',
             ),
         ),
     ),
-
-	'view_manager' => array(
-		'display_not_found_reason' => true,
-		'display_exceptions' => true,
-		'doctype' => 'HTML5',
-		'not_found_template' => 'error/404',
-		'exception_template' => 'error/index',
-		'template_map' => array(
-			'layout/layout' => __DIR__ . '/../view/layout/layout.phtml',
-			'error/404' => __DIR__ . '/../view/error/404.phtml',
-			'error/index' => __DIR__ . '/../view/error/index.phtml',
-		),
-		'template_path_stack' => array(
-			__DIR__ . '/../view',
-		),
-	),
+    'view_manager' => array(
+        'display_not_found_reason' => true,
+        'display_exceptions' => true,
+        'doctype' => 'HTML5',
+        'not_found_template' => 'error/404',
+        'exception_template' => 'error/index',
+        'template_map' => array(
+            'layout/layout' => __DIR__ . '/../view/layout/layout.phtml',
+            'error/404' => __DIR__ . '/../view/error/404.phtml',
+            'error/index' => __DIR__ . '/../view/error/index.phtml',
+        ),
+        'template_path_stack' => array(
+            __DIR__ . '/../view',
+        ),
+    ),
 );
